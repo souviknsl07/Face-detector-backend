849f131e9db74321f023831306f78d6aa186ba3a
Souvik Nath <souviknsl07@gmail.com>
2020-07-11 19:34:49 +0530

    fix

--- server.js
+++ server.js
@@ -8,15 +8,13 @@ const signin= require('./Controllers/Signin');
 const profile= require('./Controllers/Profile');
 const image= require('./Controllers/Image');
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db=knex({
 	client: 'pg',
   	connection: {
 	    connectionString : process.env.DATABASE_URL,
-	    ssl: {
-	    	rejectUnauthorized:false
-	    }
+	    ssl: true
   	}
 });
 
