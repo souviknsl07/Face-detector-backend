bb2bc7fca3bc7cf281c5b191f96ea68a2384aae1
Souvik Nath <souviknsl07@gmail.com>
2020-07-11 17:14:33 +0530

    updated connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image= require('./Controllers/Image');
 const db=knex({
 	client: 'pg',
   	connection: {
-	    host : 'postgresql-symmetrical-38924',
-	    user : 'postgres',
-	    password : 'test',
-	    database : 'smart-brain'
+	    host : process.env.DATABASE_URL,
+	    ssl: true
   	}
 });
 
