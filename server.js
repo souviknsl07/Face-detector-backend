7e81ff5e7d1e28189ce92c248c1a7f5e5709365b
Souvik Nath <souviknsl07@gmail.com>
2020-07-11 17:21:24 +0530

    updated host

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image= require('./Controllers/Image');
 const db=knex({
 	client: 'pg',
   	connection: {
-	    host : process.env.DATABASE_URL,
+	    connectionString : process.env.DATABASE_URL,
 	    ssl: true
   	}
 });
